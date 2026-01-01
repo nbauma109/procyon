ed2b3dd3ff526663e2c6de613644e2d2e83b4506
nbauma109 <nbauma109@github.com>
2026-01-01 19:19:40 +0100

    set version to 0.6.2-SNAPSHOT

--- Procyon.Core/src/main/java/com/strobel/Procyon.java
+++ Procyon.Core/src/main/java/com/strobel/Procyon.java
@@ -14,7 +14,7 @@
 package com.strobel;
 
 public final class Procyon {
-    private static final String VERSION = "1.0-SNAPSHOT";
+    private static final String VERSION = "0.6.2-SNAPSHOT";
 
     public static String version() {
         return VERSION;
